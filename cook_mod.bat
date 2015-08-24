298ea3f4e7e40028aafc8c14a32f48c07d14194d
aschleg <aaronsc32@gmail.com>
2015-08-23 17:55:25 -0700

    update paths

--- cook_mod.bat
+++ cook_mod.bat
@@ -1,7 +1,7 @@
 cd /d "E:\Program Files (x86)\Witcher 3 Mod Tools\bin\x64"
 ::cook mod
-call wcc_lite cook -platform=pc -mod="E:\Aaron\Google Drive\Projects\Mods\witcher3\scaling\uncooked\" -basedir="E:\Aaron\Google Drive\Projects\Mods\witcher3\scaling\uncooked\" -outdir="E:\Aaron\Google Drive\Projects\Mods\witcher3\scaling\cooked\"
+call wcc_lite cook -platform=pc -mod="E:\Aaron\Google Drive\Projects\Mods\witcher3\modScalingandBalance\uncooked" -basedir="E:\Aaron\Google Drive\Projects\Mods\witcher3\modScalingandBalance\uncooked" -outdir="E:\Aaron\Google Drive\Projects\Mods\witcher3\modScalingandBalance\cooked"
 ::pack mod
-call wcc_lite pack -dir="E:\Aaron\Google Drive\Projects\Mods\witcher3\scaling\cooked\" -outdir="E:\Aaron\Google Drive\Projects\Mods\witcher3\scaling\packed\modScalingBalance\content"
+call wcc_lite pack -dir="E:\Aaron\Google Drive\Projects\Mods\witcher3\modScalingandBalance\cooked" -outdir="E:\Aaron\Google Drive\Projects\Mods\witcher3\modScalingandBalance\packed\modScalingBalance\content"
 ::generate metadata.store
-call wcc_lite metadatastore -path="E:\Aaron\Google Drive\Projects\Mods\witcher3\scaling\packed\modScalingBalance\content"
+call wcc_lite metadatastore -path="E:\Aaron\Google Drive\Projects\Mods\witcher3\modScalingandBalance\packed\modScalingBalance\content"
